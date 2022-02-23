c61fb99e7310e31d883dea95c5f31b6f370b30a6
Henrik-Ehde <henrik_ehde@hotmail.com>
2022-02-23 12:55:45 +0100

    skeppet kan vrida sig

--- Space shooter Permadeath/Assets/PlayerMovement.cs
+++ Space shooter Permadeath/Assets/PlayerMovement.cs
@@ -16,7 +16,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        
+        // convert mouse position into world coordinates
+        Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // get direction you want to point at
+        Vector2 direction = (mouseScreenPosition - (Vector2)transform.position).normalized;
+
+        // set vector of transform directly
+        transform.up = direction;
     }
     void FixedUpdate()
     {
