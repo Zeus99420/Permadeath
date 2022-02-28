552afb6b6b6b3ae29c674d09a8dbc74e3ca9dea1
Zeus99420 <60926276+Zeus99420@users.noreply.github.com>
2022-02-28 14:42:04 +0100

    Skapade en ny fiende: Invader

---
+++ Space shooter Permadeath/Assets/Scripts/Invader.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invader : Enemy
+{
+    public GameObject Mastermind;
+    public GameObject projectile;
+    Rigidbody2D m_rigidbody;
+    float distanceToPlayer;
+    public float attackRange;
+    public float acceleration;
+    float nextShotTime;
+    public float cooldown;
+    public float projectileSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+            m_rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        if (player)
+        {
+            direction = ((Vector2)player.position - (Vector2)transform.position).normalized;
+
+
+            if (IsInScreen() && distanceToPlayer < attackRange && Time.time > nextShotTime)
+            {
+                nextShotTime = Time.time + cooldown;
+
+                GameObject newProjectile = Instantiate(projectile, transform.position, transform.rotation);
+                newProjectile.GetComponent<Rigidbody2D>().velocity = projectileSpeed * transform.up;
+            }
+        }
+        m_rigidbody.AddForce(direction * acceleration);
+    }
+      
+    
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
