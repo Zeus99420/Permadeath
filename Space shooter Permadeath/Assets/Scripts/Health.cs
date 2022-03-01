115551f876873fe8fa5e75588bc986579771316e
Zeus99420 <60926276+Zeus99420@users.noreply.github.com>
2022-03-01 18:43:54 +0100

    la till ny kod

---
+++ Space shooter Permadeath/Assets/Scripts/Health.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health 
+{
+    public int health;
+    public int MAXhealth;
+
+    public Health(int MAXhealth)
+    {
+        this.MAXhealth = MAXhealth;
+        health = MAXhealth;
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public void Damage(int damageAmount)
+    {
+        health -= damageAmount;
+        if (health < 0) health = 0;
+    }
+
+    public void Heal(int healAmount)
+    {
+        health += healAmount;
+        if (health < MAXhealth) health = MAXhealth;
+    }
+}
+
